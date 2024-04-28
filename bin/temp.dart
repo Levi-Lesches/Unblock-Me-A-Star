bfa1b715b2a1041ed49da21ec847fbfe84bb3569
Levi Lesches <levilesches@gmail.com>
2024-04-27 21:30:35 -0400

    Better bin/temp.dart

--- bin/temp.dart
+++ bin/temp.dart
@@ -1,14 +1,29 @@
 // ignore_for_file: avoid_print
 
+import "package:unblock/a_star.dart";
 import "package:unblock/data.dart";
 
 void main() {
+  // Solve the game
   final state = Board.test();
-  // print(state.hash());
+  final solution = aStar(state);
+  if (solution == null) {
+    print("No solution found");
+    return;
+  }
 
-  for (final next in state.expand()) {
-    print(next.transition);
-    print(next.hash());
-    print("---------------");
+  // Print start and end states
+  print("Starting with: ");
+  print(state.hash());
+  print("--------------");
+  print("Found solution: ");
+  print(solution.hash);
+
+  // Print the moves to win
+  print("--------------");
+  for (final node in solution.reconstructPath()) {
+    final move = node.state.transition;
+    if (move == null) continue;
+    print(move);
   }
 }
